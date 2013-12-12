f54ebe06f7827bcba6b21c842033fbf4385b2725
eskat0n <muyou.prj@gmail.com>
2013-12-12 15:15:10 +0600

    Change frameplate assembly name

--- source/Infrastructure.Domain/Properties/AssemblyInfo.cs
+++ source/Infrastructure.Domain/Properties/AssemblyInfo.cs
@@ -2,5 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Infrastructure.Domain")]
+[assembly: AssemblyTitle("Frameplate.Domain")]
 [assembly: Guid("4bf2ef9c-7b29-4117-ace2-79c53408612b")]
+[assembly: AssemblyCompanyAttribute("Codeparts")]
+[assembly: AssemblyProductAttribute("Frameplate")]
+[assembly: ComVisibleAttribute(false)]
